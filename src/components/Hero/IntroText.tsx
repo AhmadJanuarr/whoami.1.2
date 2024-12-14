857120cb8acdd560cb78076cb60fcad7f79c664a
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2024-12-14 20:28:54 +0700

    refactor code

--- src/components/Hero/IntroText.tsx
+++ src/components/Hero/IntroText.tsx
@@ -3,13 +3,13 @@ export default function IntroText() {
     <div className="w-full font-neuBook">
       <div className="paragraph max-w-[280px] tracking-wider lg:max-w-[650px]">
         <p>
-          👋 Hi, I&apos;m <span className="text-secondary">Ahmad Januar Amriyansah</span> 💻 A Front End Developer who
-          is passionate about building 🌐 beautiful, responsive, and user-friendly websites.
+          👋 Hi, I&apos;m <span className="font-bold text-secondary">Ahmad Januar Amriyansah</span> 💻 A Front End
+          Developer who is passionate about building 🌐 beautiful, responsive, and user-friendly websites.
         </p>
         <p className="mt-5 lg:mt-0 ">
-          ⚡ I am an expert in <span className="text-secondary">JavaScript, ⚛️</span>
-          <span className="text-secondary">ReactJS</span>, and
-          <span className="text-secondary">🚀 modern web technologies</span>
+          ⚡ I am an expert in <span className="font-bold text-secondary">JavaScript, ⚛️</span>
+          <span className="font-bold text-secondary">ReactJS</span>, and
+          <span className="font-bold text-secondary">🚀Modern web technologies</span>
           .✨
         </p>
       </div>

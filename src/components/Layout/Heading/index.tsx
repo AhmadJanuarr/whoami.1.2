cd95c4c993bde5c0a35753ebdaa6d1fa4d7edaf4
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-11 21:37:19 +0700

    refactor : define TypeScript props for Heading component

--- src/components/Layout/Heading/index.tsx
+++ src/components/Layout/Heading/index.tsx
@@ -1,4 +1,8 @@
-export default function Heading({ heading, children }: { heading: string; children: React.ReactNode }) {
+type HeadingProps = {
+  heading: string
+  children: React.ReactNode
+}
+export default function Heading({ heading, children }: HeadingProps) {
   return (
     <div className="w-full py-8">
       <h1 className="pb-2 font-neuBold text-3xl">{heading}</h1>

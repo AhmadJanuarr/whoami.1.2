b1f0a1ac228962049e67329f6be034da9a2264fc
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-11 21:36:19 +0700

    refactor : update Appshell component to use TypeScript for props and add dark mode text color

--- src/components/Layout/Appshell/index.tsx
+++ src/components/Layout/Appshell/index.tsx
@@ -2,13 +2,18 @@ import { useRouter } from "next/router"
 import Footer from "../Footer/Footer"
 import Header from "../Header/Header"
 
-export default function Appshell({ className, children }: { className: string; children: React.ReactNode }) {
+type AppShellProps = {
+  className: string
+  children: React.ReactNode
+}
+
+export default function Appshell({ className, children }: AppShellProps) {
   const { pathname } = useRouter()
   const isDetailPage = pathname.startsWith("/projects/") && pathname.split("/").length === 3
 
   return (
     <main
-      className={`${className} mx-auto h-full text-[#444444] ${!isDetailPage ? "max-w-[800px] px-5 md:px-0" : "w-full"}`}
+      className={`${className} mx-auto h-full text-[#444444] dark:text-white ${!isDetailPage ? "max-w-[800px] px-5 md:px-0" : "w-full"}`}
     >
       {!isDetailPage && <Header />}
       {children}

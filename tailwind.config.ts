5d40590df6668c2e71537faa3bdb4f456933939b
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-02-11 21:34:36 +0700

    add dark mode support to Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,32 +1,33 @@
 import type { Config } from "tailwindcss"
 
 const config: Config = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/views/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {
-            screens: {
-                sm: { min: "640px" },
-                md: { min: "768px" },
-                lg: { min: "1280px" },
-                xl: { min: "1536px" },
-            },
-            colors: {
-                primary: "#444444",
-                secondary: "#375ACB",
-            },
-            fontFamily: {
-                neuBold: ["var(--font-neu-bold)"],
-                neuMedium: ["var(--font-neu-medium)"],
-                neuBook: ["var(--font-neu-book)"],
-                neuThin: ["var(--font-neu-thin)"],
-            },
-        },
-        plugins: [],
+  darkMode: ["class"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/views/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: { min: "640px" },
+        md: { min: "768px" },
+        lg: { min: "1280px" },
+        xl: { min: "1536px" },
+      },
+      colors: {
+        primary: "#444444",
+        secondary: "#375ACB",
+      },
+      fontFamily: {
+        neuBold: ["var(--font-neu-bold)"],
+        neuMedium: ["var(--font-neu-medium)"],
+        neuBook: ["var(--font-neu-book)"],
+        neuThin: ["var(--font-neu-thin)"],
+      },
     },
+    plugins: [],
+  },
 }
 export default config

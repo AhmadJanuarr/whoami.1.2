d5e3fa8a68c73f457c4e1676dce8ef1e10120a13
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-05-17 16:08:16 +0700

    feat: create Home component to structure the main page with various sections including hero, about, features, projects, and newsletter

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { AboutPreviewSection } from "@/views/Home/AboutPreviewSection"
+import { FeatureBlogsSection } from "@/views/Home/FeatureBlogsSection"
+import { FeaturedProjectSection } from "@/views/Home/FeaturedProjectSection"
+import { HomeHeroSection } from "@/views/Home/HomeHeroSection"
+import { NewsLetterSection } from "@/views/Home/NewsLetterSection"
+
+export default function Home() {
+  return (
+    <>
+      <HomeHeroSection />
+      <AboutPreviewSection />
+      <FeatureBlogsSection />
+      <FeaturedProjectSection />
+      <NewsLetterSection />
+    </>
+  )
+}

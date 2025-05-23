24b126d9bdc06f5436d4b007fd8c59b6ba235ec6
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-05-23 10:35:39 +0700

    feat: add CardIcon component for displaying icons with links and names

---
+++ app/components/cards/CardIcon.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+
+interface CardIconProps {
+  icon: string
+  name: string
+  href: string
+}
+
+export const CardIcon = ({ icon, name, href }: CardIconProps) => {
+  return (
+    <Link className="flex flex-col items-center gap-2" href={href} target="_blank">
+      <div className="size-28 rounded-3xl border border-borderPrimary bg-white p-2 transition-all duration-300 hover:-translate-y-3 hover:border-accentColor">
+        <div className="flex h-full items-center justify-center rounded-2xl border-2 border-zinc-200 bg-[#F7F7F8]">
+          <img src={icon} alt={name} className="size-10" />
+        </div>
+      </div>
+      <p className="font-regular text-center text-sm tracking-wide text-textSecondary">{name}</p>
+    </Link>
+  )
+}

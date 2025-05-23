2324a10fdfba097117e60b6104fd688ee520e5b7
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-05-23 14:25:50 +0700

    fix: update import path for NewsLetterSection to use the correct component location

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { AboutPreviewSection } from "@/views/Home/AboutPreviewSection"
 import { FeatureBlogsSection } from "@/views/Home/FeatureBlogsSection"
 import { FeaturedProjectSection } from "@/views/Home/FeaturedProjectSection"
 import { HomeHeroSection } from "@/views/Home/HomeHeroSection"
-import { NewsLetterSection } from "@/views/Home/NewsLetterSection"
+import { NewsLetterSection } from "@/components/ui/NewsLetterSection"
 
 export default function Home() {
   return (

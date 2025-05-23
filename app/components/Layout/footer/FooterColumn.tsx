03f0562f87bb2f1ab4c75e01e2c8bc81449bd5bb
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-05-23 10:36:40 +0700

    feat: create FooterColumn component for structured footer links with conditional rendering

---
+++ app/components/Layout/footer/FooterColumn.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link"
+
+type FooterProps = {
+  title: string
+  links: string[]
+  url: string[]
+}
+
+export default function FooterColumn({ title, links, url }: FooterProps) {
+  return (
+    <div>
+      <h6 className="mb-4 font-aspekta uppercase text-textPrimary">{title}</h6>
+      <ul>
+        {links.map((item, index) =>
+          url[index] ? (
+            <Link
+              key={index}
+              href={url[index]}
+              passHref
+              target={["/", "/about", "/contact", "/projects", "/uses"].includes(url[index]) ? "_self" : "_blank"}
+            >
+              <li className="mb-5 cursor-pointer text-textSecondary hover:underline dark:text-white">{item}</li>
+            </Link>
+          ) : (
+            <li key={index} className="mb-5 text-[14px] text-textSecondary">
+              {item}
+            </li>
+          ),
+        )}
+      </ul>
+    </div>
+  )
+}

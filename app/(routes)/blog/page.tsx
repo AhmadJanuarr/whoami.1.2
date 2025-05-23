8513aa289dbbdc562c3c1df077e2a67fa6027ad3
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-05-24 02:07:57 +0700

    style: refine layout in BlogPage by adjusting section structure and removing unnecessary elements

--- app/(routes)/blog/page.tsx
+++ app/(routes)/blog/page.tsx
@@ -74,11 +74,10 @@ export default function BlogPage() {
   const filteredArticles = ARTICLE_LIST.filter((article) => article.category === selectedCategory || selectedCategory === "All")
 
   return (
-    <section className="w-full overflow-hidden bg-gray-50">
-      <div className="mb-8 bg-white px-4 py-8 text-center shadow-sm md:mb-16">
-        <motion.h1 initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} className="text-2xl font-semibold text-gray-900 md:text-4xl">
-          {heading}
-        </motion.h1>
+    <section className="w-full overflow-hidden">
+      <div className="h-14 w-full border-b" />
+      <div className="mb-8 px-4 py-8 text-center shadow-sm md:mb-16">
+        <h1 className="text-2xl font-semibold text-gray-900 md:text-4xl">{heading}</h1>
       </div>
       <div className="mx-auto max-w-7xl px-4">
         <div className="mb-6">

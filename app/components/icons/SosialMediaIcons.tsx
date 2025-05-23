623f4f7b14aa9c9034f15102d2bbbf2617b1d671
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-05-23 10:36:01 +0700

    feat: add SosialMediaIcon component for rendering social media icons with links and hover effects

---
+++ app/components/icons/SosialMediaIcons.tsx
@@ -0,0 +1,13 @@
+import { ICON_LIST } from "@/datas/icon-list"
+
+export default function SosialMediaIcon() {
+  return (
+    <div className="flex space-x-5">
+      {ICON_LIST.map(({ id, link, src: Icon, name }) => (
+        <a href={link} key={id} target="_blank" rel="noopener noreferrer">
+          <Icon className="h-[25px] w-[25px] transition-all duration-75 hover:grayscale-0 md:grayscale md:filter" />
+        </a>
+      ))}
+    </div>
+  )
+}

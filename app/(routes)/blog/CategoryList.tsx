3366f5d176a69351d9802863ef1a291381ba1d28
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-05-26 20:40:12 +0700

    feat: add CategoryList component for displaying and selecting blog categories

---
+++ app/(routes)/blog/CategoryList.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { Category, CategoryListProps } from "@/types/components"
+import { useState } from "react"
+
+export default function CategoryList({ categories }: CategoryListProps) {
+  const [selectedCategory, setSelectedCategory] = useState<Category>("All")
+  return (
+    <div className="no-scrollbar overflow-x-auto">
+      <ul className="flex gap-3 whitespace-nowrap px-1 text-sm text-gray-600">
+        {categories.map((category) => (
+          <li
+            onClick={() => setSelectedCategory(category)}
+            key={category}
+            className={`cursor-pointer rounded-full px-4 py-2 transition-all ${
+              selectedCategory === category ? "bg-accentColor text-white" : "bg-gray-100 hover:bg-gray-200"
+            }`}
+          >
+            {category}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}

cb1d81a0ef2ee9e143c309829e68525232169353
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-05-29 14:37:20 +0700

    refactor: simplify BlogPage component by integrating BlogClient and removing CategoryList and BlogCardList

--- app/(routes)/blog/page.tsx
+++ app/(routes)/blog/page.tsx
@@ -1,37 +1,8 @@
 import { getAllPosts } from "@/lib/mdx"
-import BlogCardList from "./BlogCardList"
-import CategoryList from "./CategoryList"
-import { Category } from "@/types/components"
+import BlogClient from "./BlogClient"
 
 export default function BlogPage() {
   const posts = getAllPosts()
-  const categories: Category[] = [
-    "All",
-    "Programming",
-    "Web Development",
-    "Mobile Development",
-    "UI/UX Design",
-    "Machine Learning",
-    "Artificial Intelligence",
-    "Architecture",
-  ]
-  return (
-    <section className="w-full overflow-hidden">
-      <div className="h-14 w-full border-b" />
-      <div className="mb-8 px-4 py-8 text-center shadow-sm md:mb-16">
-        <h1 className="text-2xl font-semibold text-gray-900 md:text-4xl">Exploring the World of Code: Programming Insights</h1>
-      </div>
-      <div className="mx-auto max-w-7xl px-4">
-        <div className="mb-6">
-          <h2 className="mb-3 text-lg font-medium text-gray-900">Categories</h2>
-          <CategoryList categories={categories} />
-        </div>
-        <div className="flex flex-col gap-4 pb-8">
-          {posts.map((post, index) => (
-            <BlogCardList key={post.slug} post={post} index={index} />
-          ))}
-        </div>
-      </div>
-    </section>
-  )
+
+  return <BlogClient initialPosts={posts} />
 }

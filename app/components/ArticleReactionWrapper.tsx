e224f8ebd7d1b9174f333f673e19f818bb8c2541
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-06-02 10:41:49 +0700

    refactor: update ArticleReactionWrapper to use useLikeCounter and handle likes asynchronously

--- app/components/ArticleReactionWrapper.tsx
+++ app/components/ArticleReactionWrapper.tsx
@@ -1,32 +1,46 @@
 "use client"
 
+import { ToggleLike, useLikeCounter } from "@/lib/toggle-likes"
 import { useState } from "react"
-import { PiHeartStraight, PiBookmarkSimple, PiShareFat } from "react-icons/pi"
+import { PiBookmarkSimple, PiHeartStraight, PiShareFat } from "react-icons/pi"
 
 const ReactionButton = ({ icon, text, onClick, isActive }: { icon: React.ReactNode; text: string; onClick: () => void; isActive: boolean }) => {
   return (
-    <button onClick={onClick} className={`flex items-center gap-2 rounded-full border bg-[#F1F5F9] px-4 py-2 transition-all`}>
+    <button
+      onClick={onClick}
+      className={`flex items-center gap-2 rounded-full border bg-[#F1F5F9] px-4 py-2 transition-all hover:bg-backgroundPrimary`}
+    >
       {icon}
       <span className="text-sm tracking-wider text-textSecondary">{text}</span>
     </button>
   )
 }
 
-export const ArticleReactionWrapper = () => {
-  const [liked, setLiked] = useState(false)
+export const ArticleReactionWrapper = ({ postId }: { postId: string }) => {
+  const { count: likeCount, liked } = useLikeCounter(postId)
+  const [isLiked, setIsLiked] = useState<boolean | null>(null)
   const [bookmarked, setBookmarked] = useState(false)
+
+  const handleLike = async () => {
+    const newLiked = await ToggleLike(postId)
+    setIsLiked(newLiked ?? null)
+  }
 
   return (
     <div className="flex w-full items-center justify-center">
       <div className="flex max-w-sm gap-5 rounded-full">
-        <ReactionButton icon={<PiHeartStraight size={15} />} text={liked ? "Liked" : "Like"} onClick={() => setLiked(!liked)} isActive={liked} />
+        <ReactionButton
+          icon={<PiHeartStraight size={15} className={liked ? "text-red-500" : ""} />}
+          text={isLiked ? "like" : "Likes"}
+          onClick={handleLike}
+          isActive={liked}
+        />
         <ReactionButton
           icon={<PiBookmarkSimple size={15} />}
           text={bookmarked ? "Saved" : "Save"}
           onClick={() => setBookmarked(!bookmarked)}
           isActive={bookmarked}
         />
-
         <ReactionButton icon={<PiShareFat size={15} />} text="Share" onClick={() => {}} isActive={false} />
       </div>
     </div>

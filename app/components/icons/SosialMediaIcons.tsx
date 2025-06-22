c8cf8255ac4d6e0a502bef7d2bf1254bc76fb346
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-06-22 13:49:19 +0700

    style: enhance SosialMediaIcons component for dark mode support by updating icon text color

--- app/components/icons/SosialMediaIcons.tsx
+++ app/components/icons/SosialMediaIcons.tsx
@@ -5,7 +5,7 @@ export default function SosialMediaIcon() {
     <div className="flex space-x-5">
       {ICON_LIST.map(({ id, link, src: Icon, name }) => (
         <a href={link} key={id} target="_blank" rel="noopener noreferrer">
-          <Icon className="h-[25px] w-[25px] text-textSecondary transition-all duration-75 hover:grayscale-0 md:grayscale md:filter" />
+          <Icon className="h-[25px] w-[25px] text-textSecondary transition-all duration-75 hover:grayscale-0 dark:text-textDarkSecondary md:grayscale md:filter" />
         </a>
       ))}
     </div>

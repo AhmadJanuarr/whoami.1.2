8b3898ccf34348af0abf021a43bfd96edf27e63f
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-06-22 13:54:41 +0700

    style: enhance Appshell component for dark mode support by updating border properties

--- app/components/Layout/appshell/index.tsx
+++ app/components/Layout/appshell/index.tsx
@@ -9,12 +9,12 @@ type AppShellProps = {
 
 const Appshell = ({ className, children }: AppShellProps) => {
   return (
-    <main className={`${className} dark:bg-bgDarkPrimary dark:text-textDarkPrimary relative min-h-screen overflow-hidden bg-backgroundPrimary`}>
+    <main className={`${className} relative min-h-screen overflow-hidden bg-backgroundPrimary dark:bg-bgDarkPrimary dark:text-textDarkPrimary`}>
       <div className="relative mx-auto flex min-h-screen w-full flex-col justify-center overflow-hidden border lg:max-w-[1200px] xl:max-w-[1400px]">
         <HeaderWrapper />
         <div className="grid w-full grid-cols-1 lg:grid-cols-[32px_auto_32px] xl:grid-cols-[32px_1fr_32px]">
           <div
-            className="hidden w-full border-r border-borderPrimary opacity-30 lg:block"
+            className="hidden w-full border-r border-borderPrimary opacity-30 dark:bg-borderPrimary lg:block"
             style={{
               backgroundImage:
                 "linear-gradient(45deg, #2D2D2D 12.50%, transparent 12.50%, transparent 50%, #2D2D2D 50%, #2D2D2D 62.50%, transparent 62.50%, transparent 100%)",
@@ -23,7 +23,7 @@ const Appshell = ({ className, children }: AppShellProps) => {
           />
           <div>{children}</div>
           <div
-            className="hidden w-full border-l border-borderPrimary opacity-30 lg:block"
+            className="hidden w-full border-l border-borderPrimary opacity-30 dark:bg-borderPrimary lg:block"
             style={{
               backgroundImage:
                 "linear-gradient(45deg, #2D2D2D 12.50%, transparent 12.50%, transparent 50%, #2D2D2D 50%, #2D2D2D 62.50%, transparent 62.50%, transparent 100%)",

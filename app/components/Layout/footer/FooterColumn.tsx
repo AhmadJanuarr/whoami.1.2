f2b0b1608d98f88c4be1c2a0c60ab278d2d42fcf
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-06-22 13:49:33 +0700

    style: enhance FooterColumn component for dark mode support by updating text colors for headings and list items

--- app/components/Layout/footer/FooterColumn.tsx
+++ app/components/Layout/footer/FooterColumn.tsx
@@ -9,15 +9,15 @@ type FooterProps = {
 export default function FooterColumn({ title, links, url }: FooterProps) {
   return (
     <div>
-      <h6 className="mb-4 font-aspekta uppercase text-textPrimary">{title}</h6>
+      <h6 className="mb-4 font-aspekta uppercase text-textPrimary dark:text-textDarkPrimary">{title}</h6>
       <ul>
         {links.map((item, index) =>
           url[index] ? (
             <Link key={index} href={url[index]} passHref>
-              <li className="mb-5 cursor-pointer text-textSecondary hover:underline dark:text-white">{item}</li>
+              <li className="mb-5 cursor-pointer text-textSecondary hover:underline dark:text-textDarkSecondary">{item}</li>
             </Link>
           ) : (
-            <li key={index} className="mb-5 text-[14px] text-textSecondary">
+            <li key={index} className="mb-5 text-[14px] text-textSecondary dark:text-textDarkSecondary">
               {item}
             </li>
           ),

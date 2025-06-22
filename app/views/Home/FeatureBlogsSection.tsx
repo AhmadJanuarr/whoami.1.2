216e0b2e650ebfafb7f4c3c6f848207ee928fde3
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-06-22 13:50:59 +0700

    style: enhance FeatureBlogsSection component for dark mode support by updating text color for improved visibility

--- app/views/Home/FeatureBlogsSection.tsx
+++ app/views/Home/FeatureBlogsSection.tsx
@@ -10,8 +10,8 @@ export function FeatureBlogsSection() {
       <div className="container relative mx-auto overflow-hidden">
         <div className="mb-16 text-center">
           <h2 className="border-y py-2 text-sm font-medium uppercase tracking-wider text-accentColor">Latest Articles</h2>
-          <div className="sizeTitle mx-auto flex w-full items-center justify-center border-b px-4 py-5 text-textPrimary lg:text-[40px]">
-            <h1 className="max-w-2xl"> I like to share experiences and experiments in writing.</h1>
+          <div className="sizeTitle mx-auto flex w-full items-center justify-center border-b px-4 py-5 text-textPrimary dark:text-textDarkPrimary lg:text-[40px]">
+            <h1 className="max-w-2xl "> I like to share experiences and experiments in writing.</h1>
           </div>
         </div>
         <div className="grid h-auto w-full grid-cols-1 gap-8 px-5 py-5 md:grid-cols-2 lg:grid-cols-3">

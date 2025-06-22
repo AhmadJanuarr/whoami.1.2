7a34231db6d8455593b86ca8c717fe84c97f2075
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-06-22 13:49:51 +0700

    style: enhance HeaderWrapper component for dark mode support by updating text colors and background

--- app/components/Layout/header/HeaderWrapper.tsx
+++ app/components/Layout/header/HeaderWrapper.tsx
@@ -38,11 +38,11 @@ export const HeaderWrapper = () => {
   ]
 
   return (
-    <header className="fixed top-0 z-50 w-full bg-white/80 backdrop-blur-sm lg:static">
-      <div className="grid grid-cols-2 items-center border-b border-borderPrimary px-5 py-3 text-[14px] text-textPrimary lg:grid-cols-[200px_1fr_200px] lg:px-3">
+    <header className="fixed  top-0 z-50 w-full bg-white/80 backdrop-blur-sm dark:bg-bgDarkPrimary lg:static">
+      <div className="grid grid-cols-2 items-center border-b border-borderPrimary px-5 py-3 text-[14px] text-textPrimary dark:text-textDarkPrimary lg:grid-cols-[200px_1fr_200px] lg:px-3">
         <div className="flex justify-start">
           <div className="flex items-center gap-2">
-            <img src="/icons/favicon.png" alt="logo" className="size-8" />
+            <img src="/icons/favicon.png" alt="logo" className="size-8 dark:invert" />
           </div>
         </div>
 
@@ -52,7 +52,7 @@ export const HeaderWrapper = () => {
               <li
                 key={index}
                 id={item.label.toLowerCase()}
-                className={`${item.isActive ? "border-textPrimary font-bold" : "font-medium text-textSecondary"} cursor-pointer`}
+                className={`${item.isActive ? "border-textPrimary font-bold" : "font-medium text-textSecondary dark:text-textDarkSecondary"} cursor-pointer`}
                 onClick={() => {
                   item.onClick()
                 }}
@@ -64,11 +64,9 @@ export const HeaderWrapper = () => {
         </div>
 
         <div className="hidden justify-end gap-2 md:flex">
-          <div className="flex items-center gap-2">
-            <button className="rounded-full border p-2 text-2xl" onClick={() => toggleTheme()}>
-              {theme === "light" ? <PiSun className="text-2xl" /> : <PiMoon className="text-2xl" />}
-            </button>
-          </div>
+          <button className="rounded-full border p-2 text-2xl" onClick={() => toggleTheme()}>
+            {theme === "light" ? <PiSun /> : <PiMoon />}
+          </button>
           <div className="flex gap-5 rounded-full border px-4 py-2">
             {NAV_ICONS.map((item, index) => (
               <div key={index}>{item.icon}</div>
@@ -79,7 +77,7 @@ export const HeaderWrapper = () => {
         {/* Mobile button  */}
         <div className="flex justify-end lg:hidden">
           <button className="flex items-center gap-2 rounded-full border p-2 text-2xl" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-            {isMenuOpen ? <PiX className="text-2xl" /> : <PiList className="text-2xl" />}
+            {isMenuOpen ? <PiX className="text-2xl dark:text-textDarkPrimary" /> : <PiList className="text-2xl dark:text-textDarkPrimary" />}
           </button>
         </div>
 
@@ -91,7 +89,7 @@ export const HeaderWrapper = () => {
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: -50 }}
               transition={{ duration: 0.3, ease: "easeInOut" }}
-              className="fixed left-0 top-0 -z-10 h-screen w-full border bg-backgroundPrimary backdrop-blur-sm lg:hidden"
+              className="fixed left-0 top-0 -z-10 h-screen w-full border bg-backgroundPrimary backdrop-blur-sm dark:bg-bgDarkPrimary dark:text-textDarkPrimary lg:hidden"
               onClick={() => setIsMenuOpen(false)}
             >
               <div className="flex h-full flex-col items-center justify-center">
@@ -109,8 +107,8 @@ export const HeaderWrapper = () => {
                       animate={{ y: 0, opacity: 1 }}
                       exit={{ y: -20, opacity: 0 }}
                       transition={{ duration: 0.3, delay: index * 0.1, ease: "easeInOut" }}
-                      className={`sizeTitle w-full cursor-pointer list-none border-b py-4 text-center uppercase
-                        ${item.isActive ? "font-bold text-textPrimary" : "text-textSecondary"}
+                      className={`sizeTitle w-full cursor-pointer list-none border-t py-4 text-center uppercase
+                        ${item.isActive ? "font-bold text-textPrimary dark:text-textDarkPrimary" : "text-textSecondary dark:text-textDarkSecondary"}
                       `}
                       onClick={() => {
                         item.onClick()
@@ -120,6 +118,17 @@ export const HeaderWrapper = () => {
                       {item.label}
                     </motion.li>
                   ))}
+                  <div className="flex w-full justify-center gap-2 border-t py-4">
+                    <div className="flex items-center gap-2">
+                      <button className="rounded-full border p-2" onClick={() => toggleTheme()}>
+                        {theme === "light" ? (
+                          <PiSun className="text-xl dark:text-textDarkPrimary" />
+                        ) : (
+                          <PiMoon className="text-xl dark:text-textDarkPrimary" />
+                        )}
+                      </button>
+                    </div>
+                  </div>
                 </motion.div>
               </div>
             </motion.div>

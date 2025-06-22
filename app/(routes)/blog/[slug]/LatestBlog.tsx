ebcd9b188e94f6c4406d9e3469d3861b09c02e13
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-06-22 13:47:30 +0700

    style: update LatestBlog component for dark mode support by enhancing text colors and improving layout

--- app/(routes)/blog/[slug]/LatestBlog.tsx
+++ app/(routes)/blog/[slug]/LatestBlog.tsx
@@ -12,11 +12,14 @@ export type Post = {
   content: string
 }
 
-export function LatesBlog({ latestPosts }: { latestPosts: Post[] }) {
+export function LatestBlog({ latestPosts }: { latestPosts: Post[] }) {
   return (
     <div className="grid grid-cols-1 gap-6 px-4 md:grid-cols-2 lg:grid-cols-3">
       {latestPosts.map((post) => (
-        <div key={post.slug} className="sizeSubtitle group cursor-pointer rounded-xl border border-borderPrimary bg-white">
+        <div
+          key={post.slug}
+          className="sizeSubtitle group cursor-pointer rounded-xl border border-borderPrimary bg-white dark:border-borderPrimary dark:bg-bgDarkSecondary"
+        >
           <Image
             src={post.coverImage}
             alt={post.title}
@@ -27,9 +30,11 @@ export function LatesBlog({ latestPosts }: { latestPosts: Post[] }) {
             loading="lazy"
           />
           <Link href={`/blog/${post.slug}`} className="flex flex-col p-4" scroll={true}>
-            <p className="text-sm text-textSecondary">{post.date}</p>
-            <h3 className="mt-2 h-14 font-medium text-gray-900 hover:text-accentColor group-hover:text-accentColor">{post.title}</h3>
-            <p className="text-gray-500">{post.summary}</p>
+            <p className="text-sm text-textSecondary dark:text-textDarkSecondary">{post.date}</p>
+            <h2 className="h-20 py-4 text-xl font-medium text-gray-900 hover:text-accentColor group-hover:text-accentColor dark:text-textDarkPrimary">
+              {post.title}
+            </h2>
+            <p className="text-gray-500 dark:text-textDarkSecondary">{post.summary}</p>
           </Link>
         </div>
       ))}

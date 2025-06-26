64e74009a92e94df54add1a896fd38360d3604b4
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-06-27 04:13:06 +0700

    fix: enhance text color for better visibility in SkillSection component

--- app/views/About/SkillSection.tsx
+++ app/views/About/SkillSection.tsx
@@ -6,8 +6,8 @@ export function SkillSection() {
     <section className="relative w-full overflow-hidden">
       <div className="my-10 text-center md:my-16">
         <h2 className="border-y py-2 text-sm font-medium uppercase tracking-wider text-accentColor">Abilities</h2>
-        <div className="sizeTitle mx-auto flex w-full items-center justify-center border-b py-4 text-textPrimary dark:text-textDarkPrimary md:py-5">
-          <h1 className="sizeTitle max-w-sm text-2xl">My abilities and will develop further</h1>
+        <div className="sizeTitle mx-auto flex w-full items-center justify-center border-b py-4 text-textPrimary md:py-5">
+          <h1 className="sizeTitle max-w-sm text-2xl dark:text-textDarkPrimary">My abilities and will develop further</h1>
         </div>
       </div>
       <div className="grid grid-cols-3 gap-4 px-4 sm:grid-cols-3 md:grid-cols-4 md:gap-y-8 md:px-0 lg:grid-cols-8">

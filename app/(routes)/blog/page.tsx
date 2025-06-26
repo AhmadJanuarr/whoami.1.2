e7c63c576257505f3c2df0b5260dd24d40fbe0df
AhmadJanuarr <ahmadjanuaramri2015@gmail.com>
2025-06-27 04:13:32 +0700

    chore: remove BlogPage component as part of code cleanup

--- app/(routes)/blog/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getAllPosts } from "@/lib/mdx"
-import BlogClient from "./BlogClient"
-
-export default function BlogPage() {
-  const posts = getAllPosts()
-
-  return <BlogClient initialPosts={posts} />
-}
